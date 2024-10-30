9c378192e2e7d8e6eb6e14df98d93e90c2740629
Vitor Almeida Santos <vitorsjtbarbosa@outlook.com>
2024-10-30 17:23:18 -0300

    Update App.js

--- App.js
+++ App.js
@@ -1,20 +1,5 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet } from "react-native";
-
 export default function App(){
     return(
-        <View Style={Styles.container}>
-            <text>Open up App.js to start Working on your app!</text>
-            <StatusBar style="auto" />
-        </View>
+        
     )
 }
-
-const Styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-}); 
