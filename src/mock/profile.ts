cf219db4d6eadea83ac83143b74cae3521f516de
Gabriel <gabriel.oliver0033@gmail.com>
2024-12-10 17:42:21 -0300

    Update profile.ts

--- src/mock/profile.ts
+++ src/mock/profile.ts
@@ -1,15 +1,11 @@
 export interface Profile {
     id: string;
-    name: string;
-    brand: string;
-    price: number;
-    quantity: number;
-    status: 'active' | 'inactive';
+    user: string;
+    password: string;
   }
   
   export const profile: Profile[] = [
-    { id: '1', name: 'Notebook', brand: 'Dell', price: 4500.99, quantity: 10, status: 'active' },
-    { id: '2', name: 'Smartphone', brand: 'Apple', price: 7000.00, quantity: 5, status: 'active' },
-    { id: '3', name: 'TV', brand: 'Samsung', price: 3200.50, quantity: 2, status: 'inactive' },
+    { id: '1', user: 'gabrielzinho', password: '123'},
+    { id: '2', user: 'vitinho', password: '111'},
   ];
   
