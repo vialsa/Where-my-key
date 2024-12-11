bb6012e78bb1d7ca4d6a6bc81b64a03fba643c1d
Gabriel <gabriel.oliver0033@gmail.com>
2024-12-11 13:51:03 -0300

    Adição de ScrollView e KeyboardAvoidingView

--- src/components/screens/LoginScreen/index.tsx
+++ src/components/screens/LoginScreen/index.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { View, Text, TouchableOpacity, Alert } from 'react-native';
+import {View,Text,Alert,KeyboardAvoidingView,Platform,ScrollView,TouchableWithoutFeedback,Keyboard,} from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
 import { useNavigation } from '@react-navigation/native';
 import { NativeStackNavigationProp } from '@react-navigation/native-stack';
@@ -9,7 +9,6 @@ import Button from '../../buttons/button';
 import Title from '../../titles/title';
 import Checkbox from 'expo-checkbox';
 import { useAuth } from '../../../context/authContext';
-import LoginSchema from '../../../validators/login';
 import stylesGlobal from '../../styles/global';
 import styles from './styles';
 
@@ -23,8 +22,6 @@ export default function LoginScreen() {
 
   const handleLogin = async () => {
     try {
-      await LoginSchema.validate({ username, password, keepConnected: isChecked });
-
       const success = await login({ username, password, keepConnected: isChecked });
       if (success) {
         navigation.navigate('HomeScreen');
@@ -35,36 +32,53 @@ export default function LoginScreen() {
   };
 
   return (
-    <LinearGradient
-      colors={['#A8F9FF', '#659599']}
-      start={{ x: 0, y: 0 }}
-      end={{ x: 0, y: 1 }}
-      style={stylesGlobal.container}
+    <KeyboardAvoidingView
+      style={{ flex: 1 }}
+      behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
     >
-      <Title title="Where's my key?" />
-      <Text style={styles.subtitle}>Login</Text>
-      <Input placeholder="Usuário" value={username} onChangeText={setUsername} />
-      <Input placeholder="Senha" value={password} onChangeText={setPassword} secureTextEntry />
-      <View style={styles.checkboxContainer}>
-        <Checkbox
-          value={isChecked}
-          onValueChange={setIsChecked}
-          color={isChecked ? '#32746D' : undefined}
-        />
-        <Text style={styles.checkboxText}>Manter-me conectado</Text>
-      </View>
-      <Button
-        text="Login"
-        type="primary"
-        onPress={handleLogin}
-        style={{ marginTop: 100 }}
-      />
-      <TouchableOpacity onPress={() => navigation.navigate('ForgotPasswordScreen')}>
-        <Text style={styles.forgotPassword}>Esqueci a senha</Text>
-      </TouchableOpacity>
-      <TouchableOpacity onPress={() => navigation.navigate('RegisterScreen')}>
-        <Text style={styles.register}>Cadastrar</Text>
-      </TouchableOpacity>
-    </LinearGradient>
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+        <ScrollView
+          contentContainerStyle={{ flexGrow: 1 }}
+          keyboardShouldPersistTaps="handled"
+        >
+          <LinearGradient
+            colors={['#A8F9FF', '#659599']}
+            start={{ x: 0, y: 0 }}
+            end={{ x: 0, y: 1 }}
+            style={stylesGlobal.container}
+          >
+            <Title title="Where's my key?" />
+            <Text style={styles.subtitle}>Login</Text>
+            <Input placeholder="Usuário" value={username} onChangeText={setUsername} />
+            <Input
+              placeholder="Senha"
+              value={password}
+              onChangeText={setPassword}
+              secureTextEntry
+            />
+            <View style={styles.checkboxContainer}>
+              <Checkbox
+                value={isChecked}
+                onValueChange={setIsChecked}
+                color={isChecked ? '#32746D' : undefined}
+              />
+              <Text style={styles.checkboxText}>Manter-me conectado</Text>
+            </View>
+            <Button
+              text="Login"
+              type="primary"
+              onPress={handleLogin}
+              style={{ marginTop: 100 }}
+            />
+            <TouchableWithoutFeedback onPress={() => navigation.navigate('ForgotPasswordScreen')}>
+              <Text style={styles.forgotPassword}>Esqueci a senha</Text>
+            </TouchableWithoutFeedback>
+            <TouchableWithoutFeedback onPress={() => navigation.navigate('RegisterScreen')}>
+              <Text style={styles.register}>Cadastrar</Text>
+            </TouchableWithoutFeedback>
+          </LinearGradient>
+        </ScrollView>
+      </TouchableWithoutFeedback>
+    </KeyboardAvoidingView>
   );
 }
