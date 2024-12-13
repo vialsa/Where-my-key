f700620b71770c74e6be0265eea02a07dd0de821
Vitor Almeida Santos <vitorsjtbarbosa@outlook.com>
2024-12-13 18:23:02 -0300

    register up

--- src/components/screens/RegisterScreen/index.tsx
+++ src/components/screens/RegisterScreen/index.tsx
@@ -1,11 +1,39 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, Alert } from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
 import Input from '../../inputs/input';
 import Button from '../../buttons/button';
 import stylesGlobal from '../../styles/global';
 import styles from './styles';
 import Title from '../../titles/title';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { RouteProp } from '@react-navigation/native';
+import { RootStackParamList } from '../../../navigation/routesParams';
+import { StackNavigationProp } from '@react-navigation/stack';
+
+type LoginScreenNavigationProp = StackNavigationProp<RootStackParamList, 'Register'>;
+
+export default function RegisterScreen({ navigation }: any) {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+
+  const handleRegister = async () => {
+    if (password !== confirmPassword) {
+      Alert.alert('Erro', 'As senhas não coincidem.');
+      return;
+    }
+
+    try {
+      const userObj = { username, password };
+      await AsyncStorage.setItem(username, JSON.stringify(userObj));
+      Alert.alert('Sucesso', 'Conta criada com sucesso!');
+      navigation.navigate('Login'); // Volta para a tela de login
+    } catch (e) {
+      Alert.alert('Erro', 'Erro ao registrar a conta.');
+    }
+  };
+
 
 export default function RegisterScreen() {
   return (
@@ -24,3 +52,4 @@ export default function RegisterScreen() {
     </LinearGradient>
   );
 };
+
